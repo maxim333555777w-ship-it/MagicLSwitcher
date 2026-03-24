a90bf450c19df2bfcb7138b074f20e28f5f8127b
maxim333555777w-ship-it <maxim333555777w@gmail.com>
2026-03-24 20:40:50 +0200

    Add project architecture and folder structure

---
+++ MagicLSwitcher/App/MagicLSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicLSwitcherApp.swift
+//  MagicLSwitcher
+//
+//  Created by Khachatur Abramian on 24.3.2026.
+//
+
+import SwiftUI
+
+@main
+struct MagicLSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
