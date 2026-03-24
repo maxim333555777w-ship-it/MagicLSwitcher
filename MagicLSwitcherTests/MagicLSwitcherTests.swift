167c032ae899dc26520e7c9ed6cc2a398f3c6d84
maxim333555777w-ship-it <maxim333555777w@gmail.com>
2026-03-24 12:09:51 +0200

    Initial Commit

---
+++ MagicLSwitcherTests/MagicLSwitcherTests.swift
@@ -0,0 +1,17 @@
+//
+//  MagicLSwitcherTests.swift
+//  MagicLSwitcherTests
+//
+//  Created by Khachatur Abramian on 24.3.2026.
+//
+
+import Testing
+@testable import MagicLSwitcher
+
+struct MagicLSwitcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
